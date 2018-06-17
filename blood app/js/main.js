34e72b925639cf7dea20ddf2ef08555ad7a4b024
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-06-17 11:27:22 +0500

    added files for app

--- blood app/js/main.js
+++ blood app/js/main.js
@@ -32,7 +32,7 @@ function googelSignIn(){
         console.log('email', user.email);
         console.log('name', user.displayName);
         localStorage.setItem('userObject', JSON.stringify(user));
-       if(home === user){
+       if(user === true ){
         home.style.display = 'inline';
        }
         // ...
