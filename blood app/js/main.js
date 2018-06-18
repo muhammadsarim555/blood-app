e6130afd0656d033b5b186ea4394952807f5f22d
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-06-18 19:12:01 +0500

    added files

--- blood app/js/main.js
+++ blood app/js/main.js
@@ -124,93 +124,45 @@ function googelSignIn(){
         let postErrorBloodGroup = document.getElementById('postErrorBloodGroup');
 
 
-  }
-
-
-        let modal = document.getElementById('modal');
-        modal.innerHTML = `<!-- Central Modal Medium Success -->
-        <div class="modal fade" id="centralModalSuccess" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-notify modal-success" role="document">
-                <!--Content-->
-                <div class="modal-content">
-                    <!--Header-->
-                    <div class="modal-header">
-                        <p class="heading lead">Modal Success</p>
-        
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true" class="white-text">&times;</span>
-                                                </button>
-                    </div>
-        
-                    <!--Body-->
-                    <div class="modal-body">
-                        <div class="text-center">
-                            <i class="fa fa-check fa-4x mb-3 animated rotateIn"></i>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iusto nulla aperiam
-                                blanditiis ad consequatur in dolores culpa, dignissimos, eius non possimus fugiat.
-                                Esse ratione fuga, enim, ab officiis totam.</p>
-                        </div>
-                    </div>
-        
-                    <!--Footer-->
-                    <div class="modal-footer justify-content-center">
-                        <a type="button" class="btn btn-primary">Get it now <i class="fa fa-diamond ml-1"></i></a>
-                        <a type="button" class="btn btn-outline-primary waves-effect" data-dismiss="modal">No, thanks</a>
-                    </div>
-                </div>
-                <!--/.Content-->
-            </div>
-        </div>
-        <!-- Central Modal Medium Success-->
-        
-        <!-- Button trigger modal -->
-        <div class="text-center">
-            <a href="" class="btn btn-default btn-rounded" data-toggle="modal" data-target="#centralModalSuccess">Launch Modal Success <i class="fa fa-eye ml-1"></i></a>
-        </div>`
-1
-        
-      
-      
-      
-    
-
-
-
-
-        // if(postName.length >= 3 ){
-        //   postErrorName.style.color = '#2BBBAD'
-        // postErrorName.innerHTML = 'Good!'
+  
+        if(postName.length >= 3 ){
+          postErrorName.style.color = '#2BBBAD'
+        postErrorName.innerHTML = 'Good!'
             
-        // }
+        }
         
-        //   else 
+          else 
           
-        //    { postErrorName.style.color = '#ffbb33'
-        //    postErrorName.innerHTML = 'Name must be 3 characters!'
-        // }
+           { postErrorName.style.color = '#ffbb33'
+           postErrorName.innerHTML = 'Name must be 3 characters!'
+           return false
+          
+          }
 
-        // if(postNumber.length === 11   ){
-        //   postErrorNumber.style.color = '#2BBBAD'
-        //   postErrorNumber.innerHTML = 'Good!'
-        // }
-        // else{
-        //   postErrorNumber.style.color = '#ffbb33'
-        //   postErrorNumber.innerHTML = 'numbers must be 11 '
-        // }
+        if(postNumber.length === 11   ){
+          postErrorNumber.style.color = '#2BBBAD'
+          postErrorNumber.innerHTML = 'Good!'
+        }
+        else{
+          postErrorNumber.style.color = '#ffbb33'
+          postErrorNumber.innerHTML = 'numbers must be 11 '
+          return false
+        }
         
-        // if(postBottle === " " ){
-        //   alert('good');
-          // postErrorBottles.style.color = '#2BBBAD'
-          // postErrorBottles.innerHTML = 'Good!'
-        // }
-        // else{
+        if(!postBottle === "" ){
+          alert('good');
+          postErrorBottles.style.color = '#2BBBAD'
+          postErrorBottles.innerHTML = 'Good!'
+        }
+        else{
         //  alert('d');
-          // postErrorBottles.style.color = '#ff4081'
-          // postErrorBottles.innerHTML = "Select any one"
-        // }
+        
+          postErrorBottles.style.color = '#ff4081'
+          postErrorBottles.innerHTML = "Select any one"
+        }
 
 
-      // }
+      }
     
     // else{
 
