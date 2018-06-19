9cfb888cd25385acf17c467a0fc0c79f61fe89d8
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-06-19 05:35:33 +0500

    added app files

---
+++ blood app/js/userpost.js
@@ -0,0 +1,6 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+  .register('../sw.js')
+  .then(function () { console.log('Service Worker Registered'); })
+}
+// **************************************
